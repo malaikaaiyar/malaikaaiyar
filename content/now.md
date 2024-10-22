6160af038b36e30f47b25d2d4a0c5f379e7a8738
malaikaaiyar <malaika.aiyar@standard.ai>
2024-10-22 21:28:01 +0100

    init commit

---
+++ content/now.md
@@ -0,0 +1,7 @@
+---
+title: now
+---
+
+- 2nd year CS @ Merton College Oxford
+- host of [orchard](https://lu.ma/orchard)
+- research @ [oatml](https://oatml.cs.ox.ac.uk/) doing ML & cognition stuff
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Malaika Aiyar 🌱",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
